742c837a719bf96efdd1f0f87ba569a39d9c4ed3
DarkoC98 <98darko@gmail.com>
2022-01-29 15:28:19 +0100

    Revert "Revert "Added interfaces,implementation and controller for Groups""
    This reverts commit d1fb02699c2d460a832601af24721611e4981ea3.

---
+++ BusinessLayer/Implementations/PutGroupsUsingLinq.cs
@@ -0,0 +1,36 @@
+﻿using BusinessLayer.Interface;
+using BusinessLayer.Requests;
+using DataAccessLayer;
+using DataAccessLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Implementations
+{
+    public class PutGroupsUsingLinq : IPutGroup
+    {
+        public void PutGroup(OurContext context, PutGroupRequest request)
+        {
+            var existingGroupQuery = from groups in context.groups
+                                     where groups.Id == request.Id
+                                     select groups;
+
+            var existingGroup = existingGroupQuery.FirstOrDefault();
+
+            if(existingGroup != null)
+            {
+                existingGroup.Name = request.Name;
+            }
+            else
+            {
+                Group groupForInsert = new Group();
+                groupForInsert.Name = request.Name;
+                context.groups.Add(groupForInsert);
+            }
+            context.SaveChanges();
+        }
+    }
+}
---
+++ BusinessLayer/Interface/IListGroups.cs
@@ -0,0 +1,15 @@
+﻿using DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Interface
+{
+    public interface IListGroups
+
+    {
+        public List<object> listAllGroups(OurContext context);
+    }
+}
