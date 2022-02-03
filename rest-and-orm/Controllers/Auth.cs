b47d13a475642ab4e7a7c1d52966afb81c166646
dzondra <djole.nic@gmail.com>
2022-02-03 23:51:44 +0100

    Token Manager klassa za pravljenje tokena i kontroler za login zahtev

---
+++ rest-and-orm/Controllers/Auth.cs
@@ -0,0 +1,29 @@
+﻿using DataAccessLayer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using rest_and_orm.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rest_and_orm.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Auth : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post([FromServices] OurContext context, [FromBody] LoginRequest loginRequest)
+        {
+            TokenManager tokenManager = new TokenManager(context);
+            var token = tokenManager.MakeToken(loginRequest.Username);
+            return token is null ? Unauthorized() : Ok(token);
+        }
+    }
+
+    public class LoginRequest
+    {
+        public string Username { get; set; }
+    }
+}
