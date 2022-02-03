4981a41eafb634e1799eb425fcbf3c3e2bb008b7
dzondra <djole.nic@gmail.com>
2022-02-03 23:28:35 +0100

    Unit test

---
+++ BusinessLayerTests1/Helper.cs
@@ -0,0 +1,26 @@
+﻿using DataAccessLayer;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayerTests1
+{
+    
+    public static class Helper
+    {
+        /// <summary>
+        /// Nasa metoda u helper klasi <see cref="Helper"/> koja pravi in memory (laznu) bazu
+        /// </summary>
+        public static OurContext GetFakeMemoryDB()
+        {
+            var options = new DbContextOptionsBuilder<OurContext>()
+                .UseInMemoryDatabase(databaseName: "Test")
+                .Options;
+
+            return new OurContext(options);
+        }
+    }
+}
---
+++ BusinessLayerTests1/Implementations/DeleteUserLINQTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusinessLayer.Implementations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer;
+using BusinessLayer.Exeptions;
+using Microsoft.EntityFrameworkCore;
+using DataAccessLayer.Entities;
+using BusinessLayerTests1;
+
+namespace BusinessLayer.Implementations.Tests
+{
+    [TestClass()]
+    public class DeleteUserLINQTests
+    {
+        private OurContext context = Helper.GetFakeMemoryDB();
+        private DeleteUserLINQ deleteRequest = new DeleteUserLINQ();
+
+        [TestMethod()]
+        public void Delete_User_DeletesUser()
+        {
+            // ============= PRIPREMA PODATAKA ZA TESTIRANJE (DEO 1)
+            // **** ARANGE ******
+            User userForInsert = new User
+            {
+                Id = 1,
+                Username = "Username"
+            };
+            User expectedFromDB = null;
+            context.users.Add(userForInsert);
+            context.SaveChanges();
+
+
+            // ============= IZVRASAVANJE METODE KOJA SE TESTIRA (DEO 2)
+            // **** ACT ******
+            deleteRequest.Execute(context, 1);
+
+            // ============= PROVERA -> DA LI JE STANJE KAO OCEKIVANO (DEO 3)
+            // ******** ASSERT *******
+            var deletedUserFromDB = context.users.Where(u => u.Id == 1).FirstOrDefault();
+            Assert.AreEqual(deletedUserFromDB, expectedFromDB);
+
+
+        }
+
+
+        [TestMethod()]
+        [ExpectedException(typeof(Exception), "ASD")]
+        public void Delete_User_ThrowsException()
+        {
+            this.deleteRequest.Execute(context, -2);
+        }
+    }
+}
